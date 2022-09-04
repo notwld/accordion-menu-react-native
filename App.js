99433f3cd2d74f672a6bebbcf25265b8f29ad188
M Waleed <mwfarrukh@gmail.com>
2022-09-04 23:42:51 +0500

    Initial Commit

--- App.js
+++ App.js
@@ -1,12 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native';
+import data from './data.js'
+import React from 'react'
+import { Transition,Transitioning } from 'react-native-reanimated';
+
+const transition = (
+  <Transition.Together>
+    <Transition.In type="fade" durationMs={200} />
+    <Transition.Change />
+    <Transition.Out type="fade" durationMs={200} />
+  </Transition.Together>
+);
 
 export default function App() {
+  const ref = React.useRef();
+  const [currentIndex, setcurrentIndex] = React.useState(null)
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Transitioning.View 
+    transition={transition}
+    ref={ref}
+    style={styles.container}>
+      {
+        data.map((item, index) => {
+          return (
+            <TouchableOpacity
+              style={styles.box}
+              key={index}
+              onPress={() => {
+                ref.current.animateNextTransition();
+                setcurrentIndex(index===currentIndex?null:index)
+              }}
+              activeOpacity={0.9}
+            >
+              <View style={[styles.innerbox, { backgroundColor: item.bg }]}>
+                <Text style={[styles.text, { color: item.color }]}>{item.category}</Text>
+                {index === currentIndex && <View style={styles.subList}>
+                  {
+                    item.subCategories.map((subitem, subindex) => {
+                      return (
+                        <Text style={[styles.body, { color: item.color }]} key={subindex}>{subitem}</Text>
+                      )
+                    })
+                  }
+                </View>}
+              </View>
+            </TouchableOpacity>
+          )
+        }
+        )}
+
+    </Transitioning.View>
   );
 }
 
@@ -14,7 +57,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  box: {
+    flexGrow: 1,
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: "900",
+    textTransform: "uppercase",
+    letterSpacing: -2
+  },
+  innerbox: {
+    flexGrow: 1,
+    justifyContent: "center",
+    alignItems: "center",
+
+  },
+  body: {
+    fontSize: 12,
+    lineHeight: 15 * 1.5,
+    textTransform: "uppercase",
+
+  }
 });
---
+++ data.js
@@ -0,0 +1,54 @@
+export default [
+    { bg : "purple" , color : "white", category : "Fashion" , subCategories :[
+        "Causal",
+        "Formal",
+        "Shoes",
+        "Watches",
+        "Shirts",
+        "T-Shirts",
+        "Jeans"
+    ] 
+},
+    { bg : "#00539CFF" , color : "#EEA47FFF", category : "Health Care" , subCategories :[
+        "Causal",
+        "Formal",
+        "Shoes",
+        "Watches",
+        "Shirts",
+        "T-Shirts",
+        "Jeans"
+    ] 
+},
+    { bg : "#2F3C7E" , color : "#FBEAEB", category : "Kid" , subCategories :[
+        "Causal",
+        "Formal",
+        "Shoes",
+        "Watches",
+        "Shirts",
+        "T-Shirts",
+        "Jeans"
+    ] 
+},
+    { bg : "#101820FF" , color : "#FEE715FF", category : "Men" , subCategories :[
+        "Causal",
+        "Formal",
+        "Shoes",
+        "Watches",
+        "Shirts",
+        "T-Shirts",
+        "Jeans"
+    ] 
+},
+    { bg : "#F96167" , color : "#FCE77D", category : "Women" , subCategories :[
+        "Causal",
+        "Formal",
+        "Shoes",
+        "Watches",
+        "Shirts",
+        "T-Shirts",
+        "Jeans"
+    ] 
+},
+   
+   
+]
